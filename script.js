39ac25c02ae981ca9993293f02200f97e676b40e
Muhammad Afif Naufal <muhammadafifnaufal3@gmail.com>
2025-09-15 22:27:24 +0700

    upload beberapa file untuk tugas version control system

---
+++ script.js
@@ -0,0 +1,7 @@
+function tampilkanAlert() {
+  const nama = document.getElementById("nama").textContent;
+  const kampus = document.getElementById("kampus").textContent;
+  alert("Halo, saya " + nama + " dari " + kampus);
+}
+
+console.log("File JS sudah terhubung dengan benar.");
